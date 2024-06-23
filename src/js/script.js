9c42ab55b3ee8dfab43d80a111d1273abee491ca
Zavorotna <zavorotnaa96@gmail.com>
2024-06-23 19:21:48 +0300

    v-3-front

---
+++ src/js/script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function(){
+    //color to card
+    const inputs = document.querySelectorAll('.color input[type="radio"]')
+    inputs.forEach(input => {
+      let color = input.value,
+            label = input
+      label.style.backgroundColor = color
+      label.style.border = "1px solid"
+      label.style.borderColor = color === 'white' ? '#ccc' : 'transparent'
+    })
+
+    //sub menu header
+    const menuItems = document.querySelectorAll('.down-menu'),
+        body = document.querySelector("#body")
+
+    menuItems.forEach(item => {
+        const subMenu = item.nextElementSibling
+            blackFon = item.previousElementSibling
+        //hidden body
+        item.addEventListener('mouseover', () => {
+            body.style.height = "100vh"
+            body.style.overflow = 'hidden' 
+            blackFon.style.display = 'block'
+        })
+
+        item.addEventListener('mouseout', () => {
+            blackFon.style.display = 'none'
+            body.style.overflow = ''
+            body.style.height = ''
+        })
+        //return norm view
+        subMenu.addEventListener('mouseover', () => {
+            body.style.overflow = 'hidden'
+            body.style.height = "100vh"
+            blackFon.style.display = 'block'
+        })
+        
+        subMenu.addEventListener('mouseout', () => {
+            blackFon.style.display = 'none'
+            body.style.overflow = ''
+            body.style.height = ''
+        })
+    })
+
+})
