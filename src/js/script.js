d97e964d0bfad62cfc67c75a2e6834247c5dd8e4
Zavorotna <zavorotnaa96@gmail.com>
2024-06-24 12:28:38 +0300

    v-4-front

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,12 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     //color to card
     const inputs = document.querySelectorAll('.color input[type="radio"]')
     inputs.forEach(input => {
-      let color = input.value,
+        let color = input.value,
             label = input
-      label.style.backgroundColor = color
-      label.style.border = "1px solid"
-      label.style.borderColor = color === 'white' ? '#ccc' : 'transparent'
+        label.style.backgroundColor = color
+        label.style.border = "1px solid"
+        label.style.borderColor = color === 'white' ? '#ccc' : 'transparent'
     })
 
     //sub menu header
@@ -15,11 +15,11 @@ document.addEventListener("DOMContentLoaded", function(){
 
     menuItems.forEach(item => {
         const subMenu = item.nextElementSibling
-            blackFon = item.previousElementSibling
+        blackFon = item.previousElementSibling
         //hidden body
         item.addEventListener('mouseover', () => {
             body.style.height = "100vh"
-            body.style.overflow = 'hidden' 
+            body.style.overflow = 'hidden'
             blackFon.style.display = 'block'
         })
 
@@ -34,12 +34,35 @@ document.addEventListener("DOMContentLoaded", function(){
             body.style.height = "100vh"
             blackFon.style.display = 'block'
         })
-        
+
         subMenu.addEventListener('mouseout', () => {
             blackFon.style.display = 'none'
             body.style.overflow = ''
             body.style.height = ''
         })
     })
+    // Отримання всіх пунктів підменю
+    const subMenuItems = document.querySelectorAll('.item');
+
+    // Цикл для кожного пункту підменю
+    subMenuItems.forEach(item => {
+        // Знаходження дочірнього меню
+        const subMenu = document.querySelector('.default-menu');
+
+        // Додавання події для наведення миші
+        item.addEventListener('mouseenter', function () {
+            // Сховати дочірнє меню, якщо воно є
+            if (subMenu) {
+                subMenu.style.display = 'none';
+            }
+        });
+        item.addEventListener('mouseleave', function () {
+            // Сховати дочірнє меню, якщо воно є
+            if (subMenu) {
+                subMenu.style.display = 'flex';
+            }
+        });
+    });
+
 
 })
